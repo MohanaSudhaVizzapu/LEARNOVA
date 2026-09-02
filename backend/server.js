cfbbec5f4c9c890e23b822e7b52a3aee65f90deb
MohanaSudhaVizzapu <mvizzapu1406@gmail.com>
2026-09-02 21:25:29 +0530

    Update server.js
    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ connectDB();
 
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: process.env.FRONTEND_URL || "http://localhost:5173",
         credentials: true
     })
 );
@@ -34,7 +34,6 @@ app.use(
     "/api/assignments",
     require("./routes/assignmentRoutes")
 );
-
 app.use(
     "/api/submissions",
     require("./routes/submissionRoutes")
@@ -46,7 +45,6 @@ app.use(
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
 });
-
