a6598894c722f904711ee8574ffe7c403d3604ed
MohanaSudhaVizzapu <mvizzapu1406@gmail.com>
2026-09-02 22:15:40 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,24 +8,29 @@ const connectDB = require("./config/db");
 
 const app = express();
 
+// Connect MongoDB
 connectDB();
 
+// CORS
 app.use(
     cors({
-        origin: true,
+        origin: process.env.FRONTEND_URL || "http://localhost:5173",
         credentials: true
     })
 );
 
+// Middleware
 app.use(express.json());
 app.use(cookieParser());
 
+// Test route
 app.get("/", (req, res) => {
     res.json({
         message: "Welcome to Learnova API"
     });
 });
 
+// Routes
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/courses", require("./routes/courseRoutes"));
 app.use("/api/modules", require("./routes/moduleRoutes"));
@@ -34,6 +39,7 @@ app.use("/api/assignments", require("./routes/assignmentRoutes"));
 app.use("/api/submissions", require("./routes/submissionRoutes"));
 app.use("/api/enrollments", require("./routes/enrollmentRoutes"));
 
+// Port
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, "0.0.0.0", () => {
